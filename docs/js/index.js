da52e3bec32e17b76669fd6dc86d42c205f2f65f
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-21 02:26:26 +0900

    replace demo page

---
+++ docs/js/index.js
@@ -0,0 +1,23 @@
+window.licker = window.licker || {};
+(function(ns) {
+  // hashaby.allowDomain(/0\.0\.0\.0/);
+  hashaby.allowDomain('butchi.github.io');
+  hashaby.doWith(window.licker);
+
+  ns.clearHash = function() {
+    hashaby.clearHash();
+  };
+
+  ns.showMessage = function() {
+    alert('Thanks, world!');
+  };
+
+  ns.showArg = function(arg) {
+    alert('Argument: ' + arg);
+  };
+
+  ns.showArgs = function() {
+    alert('Arguments: ' + [].join.call(arguments, ','));
+  };
+
+})(window.licker);
